56b7bde57460d68fa828dcf4ff46ada30df332c9
Jindřich Bär <jindrichbar@gmail.com>
2022-07-21 19:42:57 +0200

    feat: loading pumps from Apify dataset

--- PumpIt/App.tsx
+++ PumpIt/App.tsx
@@ -1,10 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [benziny, setBenziny] = useState(null);
+
+  // After loading the app, we want to download the dataset (and store it in `benziny`).
+  useEffect(() => {
+    fetch('https://api.apify.com/v2/datasets/KLHGYP6iynI82JxhC/items?clean=true&format=json')
+      .then((dataset) => dataset.json())
+      .then(setBenziny);
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Druhy text pod prvnim textem</Text>
+      {
+        (benziny ?? [])
+          .slice(0,10)        // we take first 10
+          .map((benzina) => ( // and render them like this...
+            <div>
+              <span>{benzina.company}</span>
+            </div>
+          ))
+      }
     </View>
   );
 }
